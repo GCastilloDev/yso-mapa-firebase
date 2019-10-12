f95829281214ae40f3163dfbdba05701e1bb3a41
tavoGeek <gcnctz@gmail.com>
2019-10-12 17:13:45 -0500

    Prueba

---
+++ objeto.js
@@ -0,0 +1,12 @@
+class documentoFirebase {
+
+    constructor(idFirestore, horaStatus, idJS, seccion, status) {
+        this.idFirestore = idFirestore;
+        this.horaStatus = horaStatus;
+        this.idJS = idJS;
+        this.seccion = seccion;
+        this.status = status;
+    }
+
+ 
+}
---
+++ token.js
@@ -0,0 +1,15 @@
+function generarToken () {
+
+    var stringLength = 15;
+
+    var stringArray = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','!','?'];
+
+    var rndString = "";
+
+    for (var i = 1; i < stringLength; i++) { 
+        var rndNum = Math.ceil(Math.random() * stringArray.length) - 1;
+        rndString = rndString + stringArray[rndNum];
+    };
+
+    return rndString;
+}
